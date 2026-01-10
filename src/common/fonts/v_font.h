72b93695379be80aab6fff4b1488d482e1bbe17d
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2026-01-09 23:05:39 -0800

    Proper StringWidth() measurement when fallbacks kick in:
    -StringWidth now uses the full-fat ParseIntoIntermediateDrawStrings() in order to get proper measurements even when some glyphs have to be substituted. Currently visible when Kreon Bold has to fall back to using Hack Bold for Esperanto symbols.

--- src/common/fonts/v_font.h
+++ src/common/fonts/v_font.h
@@ -90,6 +90,15 @@ enum EColorRange : int
 
 extern int NumTextColors;
 
+struct IntermediateDrawString
+{
+	std::basic_string<char32_t> StringUTF32;
+	Trex::ShapedGlyphs          TrexGlyphs;
+	std::vector<int>            Colors;
+	std::vector<int>            Codepoints;
+	const FFont                      *Font;
+};
+
 using GlyphSet = TMap<int, FGameTexture*>;
 
 class FFont
@@ -196,7 +205,7 @@ public:
 		return InvSupersampleFactor;
 	}
 
-	inline Trex::TextShaper* GetDynamicTextShaper()
+	inline Trex::TextShaper* GetDynamicTextShaper() const
 	{
 		return DynamicTextShaper;
 	}
@@ -277,6 +286,9 @@ protected:
 	friend void V_ClearFonts();
 	friend void V_InitFonts();
 };
+
+void ParseIntoIntermediateDrawStrings(const std::u32string_view utf32SrcString, const FFont *font, int normalcolor,
+                                      std::vector<IntermediateDrawString> &outStrings);
 
 extern FFont *SmallFont, *SmallFont2, *BigFont, *BigUpper, *ConFont, *IntermissionFont, *NewConsoleFont, *NewSmallFont, *CurrentConsoleFont, *OriginalSmallFont, *AlternativeSmallFont, *OriginalBigFont, *AlternativeBigFont;
 
