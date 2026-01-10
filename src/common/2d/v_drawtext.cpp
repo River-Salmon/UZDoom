6814f20b1dbf2a55005b2b738a7c2cfc1674d824
River-Salmon <40964273+River-Salmon@users.noreply.github.com>
2026-01-10 15:26:04 -0800

    misc cleanup of DrawText:
    -removed RedIsAlpha flag to restore LCD subpixel behavior
    -removed some red herring flags that were being set
    -included a header that was being transitively included

--- src/common/2d/v_drawtext.cpp
+++ src/common/2d/v_drawtext.cpp
@@ -59,6 +59,7 @@
 #include <vector>
 #include <string_view>
 #include "simdutf.h"
+#include "renderstyle.h"
 
 int ListGetInt(VMVa_List &tags);
 
@@ -452,8 +453,7 @@ void DrawTextCommon(F2DDrawer *drawer, FFont *font, int normalcolor, double x, d
 		
 		double cursorx = x;
 		double cursory = y;
-
-		FRenderStyle trexTextRenderStyle = {STYLEOP_Add, STYLEALPHA_Src, STYLEALPHA_One, STYLEF_RedIsAlpha | STYLEF_ColorIsFixed};
+		const FRenderStyle trexTextRenderStyle = {STYLEOP_Add, STYLEALPHA_Src, STYLEALPHA_One, STYLEF_Alpha1};
 
 		for (auto &s : DrawStrings)
 		{
@@ -478,9 +478,6 @@ void DrawTextCommon(F2DDrawer *drawer, FFont *font, int normalcolor, double x, d
 				const double srcw = (double)g.info.width / (double)atlasTexture->GetDisplayWidth();
 				const double srch = (double)g.info.height / (double)atlasTexture->GetDisplayHeight();
 				SetTextureParmsSubrect(drawer, &atlasFragmentDrawParms, atlasTexture, cx, cy, srcx, srcy, srcw, srch);
-				atlasFragmentDrawParms.masked  = false;
-				atlasFragmentDrawParms.fortext = true;
-				atlasFragmentDrawParms.bilinear   = 1;
 				atlasFragmentDrawParms.style    = trexTextRenderStyle;
 				atlasFragmentDrawParms.destwidth *= (shrinkScale);
 				atlasFragmentDrawParms.destheight *= (shrinkScale);
@@ -499,7 +496,6 @@ void DrawTextCommon(F2DDrawer *drawer, FFont *font, int normalcolor, double x, d
 					shadowAtlasFragmentDrawParms.x += (1.33*scalex*textScale);
 					shadowAtlasFragmentDrawParms.y += (1.33*scaley*textScale);
 					shadowAtlasFragmentDrawParms.color = MAKEARGB(255, 33, 33, 33);
-					shadowAtlasFragmentDrawParms.masked = 1;
 					const auto &shadowRenderStyle       = LegacyRenderStyles[10];
 					shadowAtlasFragmentDrawParms.style = shadowRenderStyle;
 					drawer->AddTexture(atlasTexture, shadowAtlasFragmentDrawParms);
